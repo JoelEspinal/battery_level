46bb35ddc66ca53b146ec4698cb5fe2114d82c73
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-06-01 04:04:39 +0000

    Call _getBatteryLevel method

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _bateryLevel;
 
+  @override
+  void initState() {
+    _getBatteryLevel();
+    super.initState();
+  }
+
   Future<void> _getBatteryLevel() async {
     const platform = MethodChannel('course.flutter.dev/battery');
     try {
